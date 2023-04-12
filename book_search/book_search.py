2a18ef08db372a2e87454741df863f87ec28e6c6
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2023-04-12 16:44:25 -0700

    Adding suggestions for improvements

---
+++ book_search/book_search.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+'''lackhack.tech | Paul Lack
+   Word search example using the input() function, 
+   opening a file, and formatting output with an fstring.
+   There are many ways this program could be improved:
+   1. Account for user typing wrongly formatted book title.
+   2. Use try/except construction to catch exceptions.
+   3. Allow user to do multiple searches.
+   4. Display a menu of available books for the user.
+   5. Allow user to search all books in the library.
+   6. Allow user to replace the searched term with another word.
+   7. Make searched terms disregard capitalization.
+   8. Add comments!
+   9. And many more...'''
+
+def main():
+    
+    count = 0
+    book = input("What book would you like to search? (moby_dick, etc.)  ")
+    term = input(f"What word or phrase would you like to search for?  ")
+    with open(f"book_files/{book}.txt", "r") as book_file:
+        for line in book_file:
+            if term in line:
+                count += 1
+    print(f'"{term}" appears {count} times in the book {book}.')
+
+if __name__ == "__main__":
+    main()
