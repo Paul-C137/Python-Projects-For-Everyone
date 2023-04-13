15fc596f9ab52d5bd76cd0a4a69afe8ba44794c9
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2023-04-13 15:49:15 -0700

    correcting error

---
+++ oswalk/oswalk_02.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''lackhack.tech | Paul Lack
+   Using os.walk() to recursively print the path and file name ror
+   every file in a directory.'''
+
+import os
+
+# get the directory to walk from the user
+dir_to_walk = input("Enter the path to the directory you wish to walk. ")
+
+# use os.walk() to recursifely walk the provided directory
+for root_path, directory, files in os.walk(dir_to_walk):
+    # files is a list so, let's sort it for fun
+    files.sort()
+    for item in files:
+        # item is a list of files that could be empty
+        # we don't want those
+        if len(item) > 0 and ".txt" in item:
+            # use os.path.join() to join the root to the file name
+            print(os.path.join(root_path, item))
+      
