d19cf45b4f850e61c3159caf4cd5d1a2e805186b
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2023-04-13 13:58:32 -0700

    Adding oswalk.py base script.

---
+++ oswalk/oswalk.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+
+import os
+
+dir_to_walk = input("Enter the path to the directory you wish to walk. ")
+
+for root_path, directory, file in os.walk(dir_to_walk):
+    print("#" * 30)
+    print("Root Directory Path ---> ", root_path)
+    print("Sub Directories ---> ", directory)
+    print("Files ---> ", file)
+    print("#" * 30)
