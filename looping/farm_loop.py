f260063a63f80015cddc2057fe04334d9f23dd41
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2023-04-23 11:48:28 -0700

    Adding simple looping example

---
+++ looping/farm_loop.py
@@ -0,0 +1,23 @@
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for i in farms[0].get("agriculture"):
+    print(i)
+
+choice = input("Please choose a farm: (NE Farm, W Farm, or SE Farm) >>> ")
+for i in farms:
+    if choice in i.values():
+        for i in i.get(f"agriculture"):
+            print(f"There are {i} on the {choice}.")
+
+vegetable_list = ["carrots", "celery"]
+
+choice = input("Please choose a farm: (NE Farm, W Farm, or SE Farm) >>> ")
+for i in farms:
+    if choice in i.values():
+        for i in i.get(f"agriculture"):
+            if i not in vegetable_list:
+                print(f"There are {i} on the {choice}.")
+            else:
+                continue
