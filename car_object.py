475342177212e9483de520ad6ac5fa77c4c64bad
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2023-05-29 15:30:35 -0700

    Adding example of an attribute without a default set.

--- car_object.py
+++ car_object.py
@@ -1,12 +1,16 @@
+# create a class constructor and define the attributes and methods
 class Car:
     def __init__(self, color='red', size='compact', year=2020):
         self.color = color
         self.size = size
         self.year = year
-
+        self.make = ''
+    
+    # the "set" methods allow us to change the attributes
     def set_color(self, color):
         self.color = color
 
+    # the "get" methods allow us to access the value of attributes
     def get_color(self):
         return self.color
 
@@ -21,11 +25,20 @@ class Car:
 
     def get_year(self):
         return self.year
+    
+    def set_make(self, make):
+        self.make = make
 
+    # some methods allow us to perform actions
     def drive_forward(self):
+        # replace this with actual logic to move x and y position
+        # of course x and y positions would have to be attributes 
+        # of the object defined above
+        # then, you could "drive forward"
         print("Car is driving forward.")
 
     def drive_backward(self):
+        # put the logic for driving backward here
         print("Car is driving backward.")
 
 
@@ -49,6 +62,9 @@ def main():
     # Drive the car forward and backward
     car1.drive_forward()
     car1.drive_backward()
+
+    car1.set_make('DeLorean')
+    print(car1.make)
 
 
 if __name__ == "__main__":
