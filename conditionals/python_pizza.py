a248d5ed10a511571cc29eefa0f5a5e9bce780ac
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2023-07-21 22:30:20 -0700

    adding another if-then conditional example.

---
+++ conditionals/python_pizza.py
@@ -0,0 +1,32 @@
+#!user/bin/python3
+
+'''This program calculates the final bill for a pizza order depending on the
+   input from the user.'''
+
+def calculate_bill(size, pepperoni, extra_cheese):
+    if size == 's':
+        bill = 15
+        if pepperoni == 'y':
+            bill += 2
+    elif size == 'm':
+        bill = 20
+        if pepperoni == 'y':
+            bill += 3 
+    else:
+        bill = 25
+        if pepperoni == 'y':
+            bill += 3
+    if extra_cheese == 'y':
+            bill += 1
+    return bill
+
+def main():
+    print('Welcome to Python Pizza!')
+    size = input('What size pizza would you like? (S,M,L) >> ').lower()
+    add_pepperoni = input('Would you like pepperoni? (Y,N)>> ')
+    add_cheese = input('Would you like extra cheese? (Y,N) >> ')
+    bill = calculate_bill(size, add_pepperoni, add_cheese)
+    print(f'Your final bill is: ${bill}.') 
+
+if __name__ == '__main__':
+    main()
