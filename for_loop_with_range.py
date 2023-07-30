00592019d473dda9629eb23b08d57799e621c6b2
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2023-07-30 07:45:27 -0700

    Adding extra to the range for loop.

--- for_loop_with_range.py
+++ for_loop_with_range.py
@@ -2,7 +2,11 @@
 
 '''Sum all the numbers from 1 - 100 by looping across a pre-defined range.'''
 
+# Start at 0
 answer = 0
+# We have to go up to but NOT including 101.  That's just how the range() 
+# function works.
 for i in range(1,101):
-    answer = answer + i
+    #answer = answer + i
+    answer += i
 print(f'The answer is: {answer}')
