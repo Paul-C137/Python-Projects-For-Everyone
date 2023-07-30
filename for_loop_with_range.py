b572e140e635481d04536cb24efd5717ed2b93d7
Paul-C137 <4980389+Paul-C137@users.noreply.github.com>
2023-07-30 07:43:07 -0700

    Adding example of a for loop with the range function.

---
+++ for_loop_with_range.py
@@ -0,0 +1,8 @@
+#!/user/bin/python3
+
+'''Sum all the numbers from 1 - 100 by looping across a pre-defined range.'''
+
+answer = 0
+for i in range(1,101):
+    answer = answer + i
+print(f'The answer is: {answer}')
