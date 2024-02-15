8d7781f659aeec62e1f7b0eceea8a6c2911e76c7
Paul-C137 <paul.lack@gmail.com>
2024-02-15 10:23:15 -0800

    adding uuid to car project

--- car_colllection_project/car_inventory.py
+++ car_colllection_project/car_inventory.py
@@ -1,28 +1,13 @@
-#!usr/bin/python3
-
-'''Jay Leno has hired you to create a Python app that will allow him
-   to inventory his cars and retrieve information about them from the
-   command line.  He was strangely insistent that you NOT use any form
-   of fancy GUI!  He wants an old-school terminal menu.
-
-   This app allows the user to Create, Read, Update, and Delete (CRUD)
-   information about cars in their private collection.  Information such
-   as Make, Model, and Year are stored in the form of a Python 
-   dictionary in a file and an excel file because Jay hates databases...
-   can't stand them.'''
-
 import json
 import pandas as pd
+import uuid
 
-# A variable in all caps is called a constant.
-# All caps tells us that this variable should not be changed elsewhere
 GREETING = '            Welcome, Jay!'
 
 def show_main_menu():
-    '''displays menu options for the user'''
-    print('*'*40)  #print the string 40 times
+    print('*'*40)
     print(GREETING)
-    print('*'*40)  #print the string 40 times
+    print('*'*40)
     print('Please select an option below:')
     print('1. Add car.')
     print('2. Find car.')
@@ -33,57 +18,73 @@ def show_main_menu():
     print('*'*40)
 
 def add_car():
-    '''builds a dictionary with the car data'''
     car_data = {}
     make = input('What is the make? >> ')
     model = input('What is the model? >> ')
     year = input('What is the year? >> ')
+    car_id = str(uuid.uuid4())[:6]
+    car_data['id'] = car_id
     car_data['make'] = make
     car_data['model'] = model
     car_data['year'] = year
     return car_data
 
+def find_car(main_list):
+    print("List of Cars:")
+    for car in main_list:
+        print(f"UUID: {car['id']}, Make: {car['make']}, Model: {car['model']}")
+
 def delete_car(main_list):
-    '''matches a car in the main dictionary and deletes it'''
-    model_to_delete = input('Enter the model of the car to delete: ')
-    found_car = None  # evaluates to False
-    for i in main_list:
-        if i['model'] == model_to_delete:
-            found_car = i
+    car_id_to_delete = input('Enter the UUID of the car to delete: ')
+    found_car = None
+    for car in main_list:
+        if car['id'] == car_id_to_delete:
+            found_car = car
             break
-    if found_car:  # evaluates to True
+    if found_car:
         main_list.remove(found_car)
-        print(f'{found_car["make"]} {found_car["model"]} {found_car["year"]} has been removed.')
+        print(f'Car with UUID "{car_id_to_delete}" has been removed.')
     else:
-        print(f'Car with model "{model_to_delete}" not found in the inventory.')
+        print(f'Car with UUID "{car_id_to_delete}" not found in the inventory.')
 
 def write_to_file(python_data):
-    '''Overwrite the old data with the newest set'''
     with open('car_data.json', 'w') as of:
         json.dump(python_data, of)
 
 def write_to_excel(python_data):
-    '''send the data to a spreadsheet (why not?)'''
     df = pd.DataFrame(python_data)
     df.to_excel('car_data.xlsx')
 
 def load_data():
-    '''get all the data from the file into the program'''
-    with open('car_data.json') as of:
-        loaded_data = json.load(of)
-    return loaded_data
-    
+    try:
+        with open('car_data.json') as of:
+            # Check if the file is empty or not
+            data = of.read().strip()
+            if not data:
+                return []  # Return an empty list if the file is empty
+            else:
+                loaded_data = json.loads(data)
+                return loaded_data
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        print("Error: The file contains invalid JSON data.")
+        return []
+
+
 def main():
     choice = " "
     main_list = load_data()
-    print(main_list)
     while choice.lower() != 'q':
         show_main_menu()
         choice = input()
         if choice.lower() == '1': 
             print('You chose to add a car.')
             main_list.append(add_car())
-        if choice.lower() == '4':
+        elif choice.lower() == '2':
+            print('You chose to find a car.')
+            find_car(main_list)
+        elif choice.lower() == '4':
             delete_car(main_list)
     write_to_file(main_list)
     write_to_excel(main_list)
