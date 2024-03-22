34457afedde557469f595f4053e1b5a2567e2bb0
Paul-C137 <paul.lack@gmail.com>
2024-03-21 17:35:04 -0700

    Adding some beginner comments.

--- try_and_except_01.py
+++ try_and_except_01.py
@@ -5,14 +5,15 @@
 PI = 3.14159
 
 def area_circle(r):
-    result = PI*r**2
-    return result
+    result = PI*r**2  #'**' is how you indicate exponents.
+    return result     #When you call the function later, you get this variable back.
 
 def main():
     try:
         #If there is an error in this block, run the except block.
         radius = float(input('Enter the radius >>> '))
-        area = area_circle(radius)
+        area = area_circle(radius) #This is where we call our custom function 
+                                   #and set 'area' equal to what it returned.
         print(f'A circle with radius {radius} has an area of {area}')
     except ValueError as val_err:
         #This block runs if an error was encountered in the try block.
