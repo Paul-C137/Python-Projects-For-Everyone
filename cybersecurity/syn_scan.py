a8fe6131245eeac3dc672ce6b07271860f57c34f
Paul-C137 <paul.lack@gmail.com>
2024-11-09 21:16:38 -0800

    add function to check if device exists before scan"

--- cybersecurity/syn_scan.py
+++ cybersecurity/syn_scan.py
@@ -1,7 +1,16 @@
 from scapy.all import *
 import ipaddress
+import warnings
+
+# Suppress ARP warning related to missing MAC address
+#warnings.filterwarnings("ignore")
 
 ports = [25,80,53,443,445,8080,8443]
+
+def is_device_present(ip):
+    arp_request = ARP(pdst=ip)
+    response = sr1(arp_request, timeout=1, verbose=0)
+    return response is not None
 
 def syn_scan(host):
 	ans, unans = sr(
@@ -12,17 +21,30 @@ def syn_scan(host):
 	print(f'Open ports at {host}:')
 	for (s,r) in ans:
 		if s[TCP].dport == r[TCP].sport and r[TCP].flags == 'SA':
-			print(s[TCP].dport)
-       
+			return s
 
 def main():
-	host = input('Enter an ip address. >> ')
-	try:
-		# raise ValueError if the argument passed is not a valid ip address
-		ipaddress.ip_address(host)
-	except:
-		print('Invalid address.')
-		exit(-1)
-	syn_scan(host)
+	device = 0
+	while device <= 255:
+		device += 1
+		host = f'192.168.222.{device}'
+		try:
+			# raise ValueError if the argument passed is not a valid ip address
+			ipaddress.ip_address(host)
+		except:
+			print('Invalid address.')
+			exit(-1)
+		# Check if the device is present
+		with warnings.catch_warnings():
+		    if is_device_present(host):
+			    source = syn_scan(host)
+			    if source:  # Ensure that 'source' is valid before trying to print
+				    try:
+					    print(source[TCP].dport)
+				    except TypeError:
+					    continue
+		    else:
+			    print(f"No response from {host}, skipping scan.")
+
 
 main()
